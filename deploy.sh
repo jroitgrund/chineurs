5048d503a3f95577af0da19705f5e5ee89c23d76
Jonathan Roitgrund <jroitgrund@palantir.com>
2016-10-22 19:07:50 +0200

    Update deploys cript for NPM

--- deploy.sh
+++ deploy.sh
@@ -9,5 +9,8 @@ git pull
 git clean -df
 source venv/bin/activate
 pip install .
+cd chineurs/frontend
+yarn install
+npm run build
 sudo systemctl restart chineurs-celery-dev.service
 sudo systemctl restart chineurs-dev.service
